2ece4328c227b465140f09794d78e138e6481bf2
isaloaiza <127746716+isaloaiza@users.noreply.github.com>
2024-03-21 16:19:56 -0500

    Update ExplicacionUser.jsx

--- client/src/routes/ExplicacionUser.jsx
+++ client/src/routes/ExplicacionUser.jsx
@@ -17,14 +17,11 @@ export default function ExplicacionUser() {
           <h1>Pasos para crear su parqueadero en ParkingLocation</h1>
           <ol>
             <li>Ingresar al sistema de ParkingLocation.</li>
-            <li>Ir al apartado de crear parqueadero.</li>
-            <li>Dar clic en el botón "Crear Parqueadero".</li>
-            <li>Llenar los datos del formulario.</li>
-            <p>
-              Para encontrar la latitud y longitud, puedes hacerlo de la siguiente forma:
-              <a href="https://coordinates-gps.gosur.com/es/" target="_blank" rel="noopener noreferrer">click aquí</a>.
-            </p>
-            <li>El Parqueadero será validado y finalmente creado.</li>
+            <li>Ir al apartado mapa de navegación.</li>
+            <li>darle click en ver info .</li>
+            <li>darle click en reservar .</li>
+          
+            <li>La reserva será validada y finalmente creado.</li>
             <div>
               <button onClick={reproducirVideo}>Ver Video</button>
             </div>
